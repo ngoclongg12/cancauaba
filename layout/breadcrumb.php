655a2711912728bc0b2b94122e70432a8f8fefc2
Phạm Ngọc Long <at150534@actvn.edu.vn>
2022-05-07 11:11:58 +0700

    main add layout breadcrumb

---
+++ layout/breadcrumb.php
@@ -0,0 +1,77 @@
+<?php
+$config = include_once $_SERVER['DOCUMENT_ROOT'] . '/cancauaba/config/config.php';
+?>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/fonts/flaticon.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/nice-select.min.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/boxicons.min.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/meanmenu.css">
+<link rel="stylesheet" type="text/css" href="<?= $config ?>/assets/css/settings.css">
+<link rel="stylesheet" type="text/css" href="<?= $config ?>/assets/css/layers.css">
+<link rel="stylesheet" type="text/css" href="<?= $config ?>/assets/css/navigation.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/modal-video.min.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/style.css">
+<link rel="stylesheet" href="<?= $config ?>/assets/css/responsive.css">
+<link rel="icon" type="image/png" href="<?= $config ?>/assets/images/favicon.png">
+
+<div class="page-title-area">
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="container">
+                <div class="title-content">
+                    <!-- tittle -->
+                    <h2>Single Product</h2>
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <!-- page -->
+                            <span>Single Product</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="title-img">
+        <img src="<?= $config ?>/assets/images/page-title2.jpg" alt="About">
+        <img src="<?= $config ?>/assets/images/shape16.png" alt="Shape">
+        <img src="<?= $config ?>/assets/images/shape17.png" alt="Shape">
+        <img src="<?= $config ?>/assets/images/shape18.png" alt="Shape">
+    </div>
+</div>
+
+<!-- content -->
+<!-- end content -->
+
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.min.js"></script>
+<script src="<?= $config ?>/assets/js/popper.min.js"></script>
+<script src="<?= $config ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?= $config ?>/assets/js/form-validator.min.js"></script>
+<script src="<?= $config ?>/assets/js/contact-form-script.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.ajaxchimp.min.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.nice-select.min.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.meanmenu.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.themepunch.tools.min.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.themepunch.revolution.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="<?= $config ?>/assets/js/extensions/revolution.extension.video.min.js"></script>
+<script src="<?= $config ?>/assets/js/jquery.mixitup.min.js"></script>
+<script src="<?= $config ?>/assets/js/owl.carousel.min.js"></script>
+<script src="<?= $config ?>/assets/js/jquery-modal-video.min.js"></script>
+<script src="<?= $config ?>/assets/js/thumb-slide.js"></script>
+<script src="<?= $config ?>/assets/js/custom.js"></script>
